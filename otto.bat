69be6a3270dd50a6db7ad3a16800db34d0832021
Salvatore Contino <salvatorecontino.consulente@euris.it>
2023-04-16 17:24:25 +0200

    modifiche ui

--- otto.bat
+++ otto.bat
@@ -1,3 +1,6 @@
 pip install -r requirements.txt
+start notepad variabili.py
+cls
+pause
 start python otto.py
 start python nove.py
--- variabili.py
+++ variabili.py
@@ -1,8 +1,8 @@
-N1 = 10 # parametro indicatore channel length
-N2 = 21 # parametro indicatore average length
-SYMBOL = "XAUUSD" # simbolo stock
-TIMEFRAME = "15m" # timeframe
-N_OPERAZIONI = 1 # numero di operazioni da aprire quando arriva il signal
-RISCHIO = 0.5 # capitale di rischio per ogni operazione
-STOPLOSS = 30 # pips di stop loss iniziale
-SPOSTAMENTO_STOPLOSS = 30 # quando il prezzo sale di 30 pips sposto lo stop loss
+N1 = 10 # ----- parametro indicatore channel length
+N2 = 21 # ----- parametro indicatore average length
+SYMBOL = "XAUUSD" # ----- simbolo stock
+TIMEFRAME = "15m" # ----- timeframe
+N_OPERAZIONI = 1 # ----- numero di operazioni da aprire quando arriva il signal
+RISCHIO = 0.5 # capitale ----- di rischio per ogni operazione
+STOPLOSS = 30 # ----- pips di stop loss iniziale
+SPOSTAMENTO_STOPLOSS = 30 # ----- quando il prezzo sale di 30 pips sposto lo stop loss
